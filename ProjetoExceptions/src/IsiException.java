d452bbd16951b535cb2f8eff5a05345f5823101b
Lucas Monteiro <lucasantoniomonteiro1@gmail.com>
2026-01-14 20:06:00 -0300

    adiciona comentario

--- ProjetoExceptions/src/IsiException.java
+++ ProjetoExceptions/src/IsiException.java
@@ -1,5 +1,6 @@
 
 //inicialmente vamos usar uma exceção não verificada
+// public class IsiException extends RunTimeException
 public class IsiException extends Exception {
 
     public IsiException(String mensagem) {
